b7071747774713836e6a4423040a4a5e8a7e6c1a
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-07-13 10:20:47 -0300

    Questão 6

--- Program.cs
+++ Program.cs
@@ -4,23 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] matriz = new int[3, 3];
-        int maior = int.MinValue;
+        string[] alunos = new string[5];
+        double[,] notas = new double[5, 3];
 
-        for (int linha = 0; linha < 3; linha++)
+        for (int i = 0; i < 5; i++)
         {
-            for (int coluna = 0; coluna < 3; coluna++)
-            {
-                Console.Write($"Digite o valor [{linha},{coluna}]: ");
-                matriz[linha, coluna] = int.Parse(Console.ReadLine()!);
+            Console.Write($"Digite o nome do {i + 1}º aluno: ");
+            alunos[i] = Console.ReadLine()!;
 
-                if (matriz[linha, coluna] > maior)
-                {
-                    maior = matriz[linha, coluna];
-                }
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Digite a {j + 1}ª nota: ");
+                notas[i, j] = double.Parse(Console.ReadLine()!);
             }
         }
 
-        Console.WriteLine($"\nMaior valor: {maior}");
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            double media = (notas[i, 0] + notas[i, 1] + notas[i, 2]) / 3;
+            string situacao;
+
+            if (media >= 7)
+            {
+                situacao = "Aprovado";
+            }
+            else if (media >= 5)
+            {
+                situacao = "Recuperação";
+            }
+            else
+            {
+                situacao = "Reprovado";
+            }
+
+            Console.WriteLine($"Aluno: {alunos[i]}");
+            Console.WriteLine($"Média: {media:F2}");
+            Console.WriteLine($"Situação: {situacao}");
+            Console.WriteLine();
+        }
     }
 }
