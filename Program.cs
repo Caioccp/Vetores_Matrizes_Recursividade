d18e9f3dc78d3c30c919eba313eefa9ff4de09b2
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-07-13 10:01:30 -0300

    Questão 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] alunos = new string[5];
+
+        for (int i = 0; i < alunos.Length; i++)
+        {
+            Console.Write($"Digite o nome do {i + 1}º aluno: ");
+            alunos[i] = Console.ReadLine()!;
+        }
+
+        Console.WriteLine("\nAlunos cadastrados:");
+
+        for (int i = 0; i < alunos.Length; i++)
+        {
+            Console.WriteLine(alunos[i]);
+        }
+    }
+}
