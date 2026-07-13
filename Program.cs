57ff0240d01cd10bd4f9ecfccfe82be2ac068e8a
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-07-13 10:27:49 -0300

    Questão 8

--- Program.cs
+++ Program.cs
@@ -4,31 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] presenca = new int[3, 5];
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine()!);
 
-        for (int aluno = 0; aluno < 3; aluno++)
+        Console.WriteLine($"Soma: {SomarAteN(numero)}");
+    }
+
+    static int SomarAteN(int n)
+    {
+        if (n > 1)
         {
-            Console.WriteLine($"Aluno {aluno + 1}");
-
-            for (int dia = 0; dia < 5; dia++)
-            {
-                Console.Write($"Dia {dia + 1} (1 = Presente / 0 = Ausente): ");
-                presenca[aluno, dia] = int.Parse(Console.ReadLine()!);
-            }
-
-            Console.WriteLine();
+            return n + SomarAteN(n - 1);
         }
 
-        for (int aluno = 0; aluno < 3; aluno++)
-        {
-            int total = 0;
-
-            for (int dia = 0; dia < 5; dia++)
-            {
-                total += presenca[aluno, dia];
-            }
-
-            Console.WriteLine($"Aluno {aluno + 1}: {total} presenças");
-        }
+        return n;
     }
 }
